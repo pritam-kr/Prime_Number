27441eef8eff159f7ffde2b05be493c558fae981
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-08-10 22:10:28 +0530

    changes something

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const month = document.querySelector("#month");
 const dateOfBirth = document.querySelector("#date-birth");
 let isPrime = true;
 
+function checkDate(date) {
+  if (date > 31) {
+    document.querySelector(".out-put").innerHTML = `<p>Enter valid date. </p>`;
+  } else {
+    checkPrime(date);
+  }
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   var nameValue = myName.value;
   var dateValue = Number(dateOfBirth.value);
+  checkDate(dateValue)
+
   var monthValue = Number(month.value);
-  checkPrime(dateValue);
-  if (dateValue > 31) {
-    document.querySelector(".out-put").innerHTML = `<p>Enter valid date. </p>`;
-  } else if (!nameValue) {
+
+  if (!nameValue) {
     document.querySelector(
       ".out-put"
     ).innerHTML = `<p>Name field can not blank.</p>`;
@@ -20,21 +28,18 @@ form.addEventListener("submit", (e) => {
     document.querySelector(
       ".out-put"
     ).innerHTML = `<p>Date field can not blank.</p>`;
-  }
-});
-
-function checkMonth(e) {
-  if (!e) {
+  } else if (!monthValue) {
     document.querySelector(
       ".out-put"
     ).innerHTML = `<p>Month field can not blank.</p>`;
-  } else if (e > 12) {
+  } else if (monthValue > 12) {
     document.querySelector(".out-put").innerHTML = `<p>Enter valid month. </p>`;
   }
-}
+
+});
+
 
 //check prime
-
 function checkPrime(e) {
   // console.log(e)
   if (e === 1) {
@@ -43,7 +48,7 @@ function checkPrime(e) {
     ).innerHTML = `<p>Your birth date is neither prime a composite number. </p>`;
   } else if (e > 1) {
     for (let i = 2; i < e; i++) {
-    //   console.log(i);
+      //   console.log(i);
       if (e % i == 0) {
         isPrime = false;
         break;
